64caa1b1a760be93693c65e62f50ddd64c934c40
Assnsster <anous123bg@gmail.com>
2024-08-10 15:38:55 +0700

    Update InsIsDoneTRun.sh

--- InsIsDoneTRun.sh
+++ InsIsDoneTRun.sh
@@ -16,10 +16,22 @@ if [ ! -x ~/vps/ngrok "
 then
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz -O ~/vps/ngrok.tgz
 fi
-
 tar -xvf ngrok.tgz
 chmod +x ngrok
-
+read -p "Ngrok Authtoken Here: " nngrok
+./ngrok authtoken $nngrok
+clear
+echo PORT:
+echo """3389) Remote Desktop - RDP (Connect if Windows start )
+5905) VNC - VNC Viewer
+8006) NoVNC - Web
+read -p " PORT: " Pos
+nohup ./ngrok tcp $Pos &>/dev/null &
+echo Start Ngrok...
+sleep 6
+echo "Your ngrok Address:"
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+}
 #####
 tailscaleins()
 {
